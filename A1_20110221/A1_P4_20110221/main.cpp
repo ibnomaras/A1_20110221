45cbf00ec9741a22d00e38865f122c657da6c5f4
ibnomaras <ibnomaras@gmail.com>
2017-02-26 10:59:59 +0200

    P4 completion

--- A1_20110221/A1_P4_20110221/main.cpp
+++ A1_20110221/A1_P4_20110221/main.cpp
@@ -2,8 +2,29 @@
 
 using namespace std;
 
+int power (int a , int n)
+{
+    if(n==1) return a ;
+    else
+    {
+        return a * power(a,n-1);
+    }
+}
+
+int power2 (int a , int n)
+{
+    if(n==1) return a ;
+    else
+    {
+        return power(a,n/2) * power(a,n/2+n%2);
+    }
+}
+
+
 int main()
 {
     cout << "Hello world!" << endl;
+    cout << power(2,3) <<endl;
+    cout << power2(2,3);
     return 0;
 }
